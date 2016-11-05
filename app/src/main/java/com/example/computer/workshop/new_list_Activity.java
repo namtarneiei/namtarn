daab907bb0a8ff8c54f82054a62ad42b2b8bbff5
namtarneiei <sirilakjanlaksa@gmail.com>
2016-11-05 11:35:51 +0700

    First commit

---
+++ app/src/main/java/com/example/computer/workshop/new_list_Activity.java
@@ -0,0 +1,13 @@
+package com.example.computer.workshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class new_list_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_list_);
+    }
+}
